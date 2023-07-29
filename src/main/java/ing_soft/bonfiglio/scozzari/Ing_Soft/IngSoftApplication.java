8882d1e0b998ed326ba68f8562dca9d50e7877de
mattia757 <mattiabonfiglio1@gmail.com>
2023-07-29 11:54:58 +0200

    Import project

---
+++ src/main/java/ing_soft/bonfiglio/scozzari/Ing_Soft/IngSoftApplication.java
@@ -0,0 +1,13 @@
+package ing_soft.bonfiglio.scozzari.Ing_Soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngSoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ing_soft/bonfiglio/scozzari/Ing_Soft/IngSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package ing_soft.bonfiglio.scozzari.Ing_Soft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
