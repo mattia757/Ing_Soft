052ded7de64b9b0ac0a55119dc8bf53f0e81b865
Kevi0 <kscozzari03@gmail.com>
2023-10-06 20:03:35 +0200

    InputDTO

--- src/main/java/ing_soft/bonfiglio/scozzari/Ing_Soft/dto/inputDTO/InputDTO.java
+++ src/main/java/ing_soft/bonfiglio/scozzari/Ing_Soft/dto/inputDTO/InputDTO.java
@@ -1,4 +1,15 @@
 package ing_soft.bonfiglio.scozzari.Ing_Soft.dto.inputDTO;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "CustomType")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = RegistrationArtistDTO.class, name = "registrationArtist"),
+        @JsonSubTypes.Type(value = TheaterDTO.class, name = "registrationTheater"),
+})
 public interface InputDTO {
 }
